e628cd7d72f50c757cfefc1ec0bca3180fb18174
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-08-08 23:02:07 +0530

    basic project setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="grid place-items-center h-screen">
+        Vite is good
+        <Button variant="outline">Button</Button>
+
+      </div>
+    </>
+  )
+}
+
+export default App
